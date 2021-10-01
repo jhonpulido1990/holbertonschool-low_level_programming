0b521b01b969ba6ec65d5a237187e7091e495b62
jhonpulido1990 <3825@holbertonschool.com>
2021-10-01 13:04:35 -0700

    learning c programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase
+*
+*Return: 0
+*/
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar(10);
+	return (0);
+}
