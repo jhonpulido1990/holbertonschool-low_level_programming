6c4b8f29a3b8699efe30ad52f18e0af0a8f56908
jhonpulido1990 <3825@holbertonschool.com>
2021-10-06 15:33:52 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,5 +6,9 @@
 void positive_or_negative(int i);
 /*program prints the largest of three integers*/
 int largest_number(int a, int b, int c);
+/*prints how many days are left in the year*/
+int convert_day(int month, int day);
+/*takes a date and prints how many days*/
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
