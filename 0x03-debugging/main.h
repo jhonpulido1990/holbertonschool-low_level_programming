920db9cb8c253bc06b4e8276e2675174f9880d94
jhonpulido1990 <3825@holbertonschool.com>
2021-10-06 14:13:52 -0700

    debuging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+/*positive or negative*/
 void positive_or_negative(int i);
+/*program prints the largest of three integers*/
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
