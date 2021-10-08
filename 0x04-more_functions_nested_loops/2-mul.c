80d66576e98115a2ac1b5c4f2a6b1e353f297d33
jhonpulido1990 <3825@holbertonschool.com>
2021-10-08 11:34:25 -0500

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ * @a: data integer
+ * @b: data integer
+ * Return: data
+ */
+int mul(int a, int b)
+{
+	int c = 0;
+
+	c = a * b;
+	return (c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,5 +9,7 @@ int _putchar(char c);
 int _isupper(int c);
 /*function that checks for a digit (0 through 9)*/
 int _isdigit(int c);
+/*function that multiplies two integers*/
+int mul(int a, int b);
 
 #endif /*HEADER_FILE*/
