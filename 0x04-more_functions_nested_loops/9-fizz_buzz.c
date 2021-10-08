ada5a9a5cdf18423690223d5b9ad6e2cb6cebaac
jhonpulido1990 <3825@holbertonschool.com>
2021-10-08 14:33:36 -0500

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - print FizzBuzz
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 15) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if ((a % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((a % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", a);
+		}
+		if (a < 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
