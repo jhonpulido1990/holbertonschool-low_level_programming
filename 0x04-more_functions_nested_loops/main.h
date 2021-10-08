c889188330a3969c1f302eaa4a88a132d216900a
jhonpulido1990 <3825@holbertonschool.com>
2021-10-08 11:04:14 -0500

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/*include library*/
+#include <unistd.h>
+#include <stdio.h>
+/*call a funtion _putchar*/
+int _putchar(char c);
+/*function that checks for uppercase character*/
+int _isupper(int c);
+/*function that checks for a digit (0 through 9)*/
+int _isdigit(int c);
+
+#endif /*HEADER_FILE*/
