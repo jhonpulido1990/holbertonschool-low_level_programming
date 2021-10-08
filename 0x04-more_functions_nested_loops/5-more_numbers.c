17676692dd71b82d2f44bc83fc0c29971c7c03ce
jhonpulido1990 <3825@holbertonschool.com>
2021-10-08 12:47:31 -0500

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,8 @@ void more_numbers(void)
 			if (b >= 10)
 			{
 				_putchar((b / 10) + '0');
-				_putchar((b % 10) + '0');
 			}
-			else
-			{
 			_putchar(b + '0');
-			}
 		}
 		_putchar(10);
 	}
