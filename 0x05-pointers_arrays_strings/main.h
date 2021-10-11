691a38260c8ad047f7eda5d61a5bd0e16aee37d7
jhonpulido1990 <3825@holbertonschool.com>
2021-10-11 16:07:41 -0500

    updates the value it points to to 98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/*include library*/
+#include <unistd.h>
 #include <stdio.h>
+/*call a funtion _putchar*/
+int _putchar(char c);
+/*updates the value it points to to 98.*/
+void reset_to_98(int *n);
 /*function that swaps the values of two integers*/
 void swap_int(int *a, int *b);
 /*function that returns the length of a string*/
