9bd6e4458aecc4038394001defa05b3fcde5115c
jhonpulido1990 <3825@holbertonschool.com>
2021-10-14 15:08:41 -0500

    changes all lowercase letters of a string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * 
+ * 
+ * 
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,5 +15,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 /*function that reverses the content of an array of integers*/
 void reverse_array(int *a, int n);
+/*changes all lowercase letters of a string to uppercase*/
+char *string_toupper(char *);
 
 #endif /* MAIN_H */
