3173bf102cb7cae3a51797d173ddf746b0044c25
jhonpulido1990 <3825@holbertonschool.com>
2021-10-18 16:08:07 -0500

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * 
+ * 
+ * 
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n > 0)
+	{
+		*ptr = b;
+		ptr++;
+		n--;
+	}
+	return (s);
+}
