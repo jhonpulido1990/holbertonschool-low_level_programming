d35621b4de85da1f04e364084a82ed8250de0159
jhonpulido1990 <3825@holbertonschool.com>
2021-10-19 14:04:43 -0500

    function that locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * 
- * 
- * 
- * 
+ * _strstr - function that locates a substring
+ * @haystack: This is the main C string to be scanned.
+ * @needle: This is the small string to be searched with-in haystack string.
+ * Return: haystack or null
  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack != '\0')
+	for (; *haystack != '\0'; haystack++)
 	{
 			if (*haystack == *needle)
 			{
 				return (haystack);
 				needle++;
 			}
-		haystack++;
 	}
 	if (*haystack != *needle)
 	{
