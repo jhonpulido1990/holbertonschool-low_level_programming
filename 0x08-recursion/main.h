830f62d73576db8e2781b71c82c0ae8db9227a49
jhonpulido1990 <3825@holbertonschool.com>
2021-10-20 10:16:39 -0500

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,23 +5,23 @@
 #include <stdio.h>
 /*call a funtion _putchar*/
 int _putchar(char c);
-/*Write a function that fills memory with a constant byte*/
-char *_memset(char *s, char b, unsigned int n);
-/*Write a function that copies memory area.*/
-char *_memcpy(char *dest, char *src, unsigned int n);
-/*Write a function that locates a character in a string*/
-char *_strchr(char *s, char c);
-/*Write a function that gets the length of a prefix substring*/
-unsigned int _strspn(char *s, char *accept);
-/*Write a function that searches a string for any of a set of bytes*/
-char *_strpbrk(char *s, char *accept);
-/*Write a function that locates a substring*/
-char *_strstr(char *haystack, char *needle);
-/*Write a function that prints the chessboard*/
-void print_chessboard(char (*a)[8]);
-/*prints the sum of the two diagonals of a square matrix of integers*/
-void print_diagsums(int *a, int size);
-/*Write a function that sets the value of a pointer to a char*/
-void set_string(char **s, char *to);
+/*Write a function that prints a string, followed by a new line*/
+void _puts_recursion(char *s);
+/*Write a function that prints a string in reverse*/
+void _print_rev_recursion(char *s);
+/*Write a function that returns the length of a string.*/
+int _strlen_recursion(char *s);
+/*Write a function that returns the factorial of a given number*/
+int factorial(int n);
+/*Write a function that returns the value of x raised to the power of y*/
+int _pow_recursion(int x, int y);
+/*Write a function that returns the natural square root of a number.*/
+int _sqrt_recursion(int n);
+/*returns 1 if the input integer is a prime number, otherwise return 0*/
+int is_prime_number(int n);
+/*Write a function that returns 1 if a string is a palindrome and 0 if not*/
+int is_palindrome(char *s);
+/*compar two string and returns 1 if the string can be considered identical*/
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
