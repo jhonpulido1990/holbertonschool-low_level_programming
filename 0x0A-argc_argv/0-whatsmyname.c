fd38488dbbc62312c0d01a34e6c01bbbfc286acc
jhonpulido1990 <3825@holbertonschool.com>
2021-10-22 13:25:08 -0500

    rogram that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,12 +11,9 @@ int main(int argc, char *argv[])
 
 	printf("%s\n", argv[0]);
 
-	if (argc > 1)
-	{
-		for (count = 1; count < argc; count++)
+		for (; count < argc; count++)
 		{
 			printf("argv[%d] = %s\n", count, argv[count]);
 		}
-	}
 	return (0);
 }
