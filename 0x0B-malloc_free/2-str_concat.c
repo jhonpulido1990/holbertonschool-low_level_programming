4950ec1df22f0b8f9e6c391d343299eff2d492a7
jhonpulido1990 <3825@holbertonschool.com>
2021-10-26 17:26:49 -0500

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-str_concat.c -o 2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,9 +10,13 @@ char *str_concat(char *s1, char *s2)
 	int i, j, p, k, size;
 	char *ptr1;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	for (i = 0; s1[i]; i++)
 	{
