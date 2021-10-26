8dc20d6f43152d455bd7c0082418e26b9b20ccf7
jhonpulido1990 <3825@holbertonschool.com>
2021-10-26 16:18:27 -0500

    returns a pointer to a newly allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,15 +10,19 @@ char *_strdup(char *str)
 	int a;
 	char *ptr1;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
-	ptr1 = (char *)malloc(i * sizeof(char));
+	ptr1 = (char *)malloc((i + 1) * sizeof(char));
 	if (ptr1 == NULL)
 	{
 		return (NULL);
 	}
-	for (a = 0;a < i; a++)
+	for (a = 0; a < i; a++)
 	{
 		ptr1[a] = str[a];
 	}
