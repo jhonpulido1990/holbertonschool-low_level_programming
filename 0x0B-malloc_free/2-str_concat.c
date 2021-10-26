a1d93d15710eb3a993c58e0655c995d475d01e0c
jhonpulido1990 <3825@holbertonschool.com>
2021-10-26 17:22:18 -0500

    concated two string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * str_concat - concated two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: strings cocated
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, p, k, size;
+	char *ptr1;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i]; i++)
+	{
+	}
+	for (j = 0; s2[j]; j++)
+	{
+	}
+	size = i + j;
+	ptr1 = (char *)malloc((size + 1) * sizeof(char));
+	for (p = 0; p < i; p++)
+	{
+		if (s1[p] != '\0')
+		{
+			ptr1[p] = s1[p];
+		}
+	}
+	for (k = 0; p < size; k++)
+	{
+		ptr1[p] = s2[k];
+		p++;
+	}
+	ptr1[p] = '\0';
+	return (ptr1);
+}
