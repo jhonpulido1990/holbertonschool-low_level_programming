26bae105cdf435c787eb393dfce5af43bfe4cc7d
jhonpulido1990 <3825@holbertonschool.com>
2021-10-26 19:02:48 -0500

    pointer to a 2 dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,19 +8,20 @@
 int **alloc_grid(int width, int height)
 {
 	int i = 0;
-	int **alloc_grid = (int **)malloc(width * sizeof(int *));
+	int **alloc_grid;
 
-	if (alloc_grid == NULL)
-	{
-		return (NULL);
-	}
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < width; i++)
+	alloc_grid = (int **)malloc(height * sizeof(int *));
+	if (alloc_grid == NULL)
 	{
-		alloc_grid[i] = (int *)malloc(height * sizeof(int));
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		alloc_grid[i] = (int *)malloc(width * sizeof(int));
 	}
 	if (alloc_grid == NULL)
 	{
