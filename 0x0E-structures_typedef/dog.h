da4a9049204ce2ac1fc44e06c9eef6a6edefb731
jhonpulido1990 <3825@holbertonschool.com>
2021-11-01 18:22:10 -0500

    struct of class

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MYDOG
+#define MYDOG
+/**
+ * struct dog - class of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*MYDOG*/
