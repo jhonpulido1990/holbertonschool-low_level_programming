8f66cb3842196983d27b99621b7cd351d8ff4ac2
jhonpulido1990 <3825@holbertonschool.com>
2021-11-01 19:40:26 -0500

    add comment

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,5 +16,5 @@ void print_dog(struct dog *d)
 	{
 		printf("NULL");
 	}
-	printf("%s\n%f\n%s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
