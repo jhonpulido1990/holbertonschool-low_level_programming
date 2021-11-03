e79da288a9e9bdd8b6c312bd1156fe371736f60f
jhonpulido1990 <3825@holbertonschool.com>
2021-11-03 16:18:40 -0500

    change condition if

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,15 +9,16 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	if (array == NULL && action == NULL && size == '\0')
-	{
-	}
-	else
+	if (array != NULL && action != NULL && size != '\0')
 	{
 		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
 		}
+	}
+	else
+	{
+		return;
 	}
 
 }
