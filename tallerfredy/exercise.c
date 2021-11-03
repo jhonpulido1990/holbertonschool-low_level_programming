976bac8bf70c2b52f6d7b2b518e795064fbf82af
jhonpulido1990 <3825@holbertonschool.com>
2021-11-03 14:28:38 -0500

    ejerciciosfredy

---
+++ tallerfredy/exercise.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - list of person
+ */
+void main(void)
+{
+	struct Person
+
+	{
+		char *name;
+		int age;
+	};
+	struct Person PeopleList[5] = {
+		{"Fredy", 27},
+		{"Jhon", 24},
+		{"Kevin", 45},
+		{"Chris", 12},
+		'\0'};
+
+}
