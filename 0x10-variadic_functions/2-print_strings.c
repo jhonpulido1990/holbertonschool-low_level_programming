6b67833349f1c03e63b546b03628e5a30b6b08af
jhonpulido1990 <3825@holbertonschool.com>
2021-11-04 15:31:28 -0500

    delect one declaration in condition if

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,10 +17,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%s", va_arg(ap, char*));
-		if (i < (n - 1))
-		{
-			printf("%s", separator);
-		}
 	}
 	printf("\n");
 	va_end(ap);
