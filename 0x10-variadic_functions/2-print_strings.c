9f9551c167238f76093d23bc9a7020cb7a01c5a2
jhonpulido1990 <3825@holbertonschool.com>
2021-11-04 16:33:32 -0500

    correction output

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,8 +1,9 @@
 #include "variadic_functions.h"
 /**
- * print_strings - function that prints strings
- * @separator: string input
- * @n: data
+ * print_strings - function that prints strings, followed by a new line.
+
+ *  
+ * 
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
@@ -17,6 +18,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%s", va_arg(ap, char*));
+		if (i < (n - 1))
+		{
+			printf("%s", separator);
+		}
 	}
 	printf("\n");
 	va_end(ap);
