48fc7e9aae80d73c6833247112362057ec476931
jhonpulido1990 <3825@holbertonschool.com>
2021-11-04 21:51:16 -0500

    correction condition in state for

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,11 +8,20 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
+	char *p;
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(ap, char*));
+		p = va_arg(ap, char*);
+		if (p != NULL)
+		{
+			printf("%s", p);
+		}
+		else
+		{
+			printf("(nil)");
+		}
 		if (i < (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
