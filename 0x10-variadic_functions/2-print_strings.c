ab8ed7d927a3cdd9ae55680cda40630422b4e1e5
jhonpulido1990 <3825@holbertonschool.com>
2021-11-04 21:38:06 -0500

    correction condition if to variable separator

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,14 +10,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
-	if (separator == NULL)
-	{
-		printf(" ");
-	}
 	for (i = 0; i < n; i++)
 	{
 		printf("%s", va_arg(ap, char*));
-		if (i < (n - 1))
+		if (i < (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
