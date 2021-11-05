ef10591300d2bc1f9948693a70e47b464a1b342b
jhonpulido1990 <3825@holbertonschool.com>
2021-11-04 21:34:04 -0500

    change of condition if to separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,16 +8,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-
 	va_start(ap, n);
-	if (separator == NULL)
-	{
-		printf(" ");
-	}
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if (i < (n - 1))
+		if (i < (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
