c614bc18206c0abd39ba80785e43eaa6fc6fe951
jhonpulido1990 <3825@holbertonschool.com>
2021-11-11 21:55:00 -0500

    excesice task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,19 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/*call a funtion _putchar*/
-int _putchar(char c);
-/*prints all the elements of a list_t list.*/
-size_t print_list(const list_t *h);
-/*the number of elements in a linked list_t list.*/
-/*size_t list_len(const list_t *h);*/
-/*adds a new node at the beginning of a list_t list.*/
-/*list_t *add_node(list_t **head, const char *str);*/
-/*adds a new node at the end of a list_t list.*/
-/*list_t *add_node_end(list_t **head, const char *str);*/
-/*Write a function that frees a list_t list.*/
-/*void free_list(list_t *head);*/
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -32,5 +19,17 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+/*call a funtion _putchar*/
+int _putchar(char c);
+/*prints all the elements of a list_t list.*/
+size_t print_list(const list_t *h);
+/*the number of elements in a linked list_t list.*/
+/*size_t list_len(const list_t *h);*/
+/*adds a new node at the beginning of a list_t list.*/
+/*list_t *add_node(list_t **head, const char *str);*/
+/*adds a new node at the end of a list_t list.*/
+/*list_t *add_node_end(list_t **head, const char *str);*/
+/*Write a function that frees a list_t list.*/
+/*void free_list(list_t *head);*/
 
 #endif /* MAIN_H */
