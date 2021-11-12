fc7a9bd8c71d5923d66c93968c8aaeee96d6330e
jhonpulido1990 <3825@holbertonschool.com>
2021-11-11 22:25:27 -0500

    leng data structure

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * list_len - number of elements in a linked list_t list
+ * @h: input data
+ * Return: leng string
+ */
+size_t list_len(const list_t *h)
+{
+	int a = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			h = h->next;
+			a++;
+		}
+		else
+		{
+			h = h->next;
+			a++;
+		}
+	}
+	return (a);
+}
