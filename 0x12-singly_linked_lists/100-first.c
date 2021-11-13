fcc583934eed405a9449662117a07cb9f6c8938d
jhonpulido1990 <3825@holbertonschool.com>
2021-11-12 23:53:31 -0500

    correction betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,11 @@
-#include "lists.h"
-/**
- * @brief 
- * 
- */
 #include<stdio.h>
-  
-/* Apply the constructor attribute to myStartupFun() so that it
-    is executed before main() */
-void myStartupFun (void) __attribute__ ((constructor));
+/**
+ * myStartupFun - execute before main()
+ */
+void myStartupFun(void) __attribute__ ((constructor));
 /* implementation of myStartupFun */
-void myStartupFun (void)
+void myStartupFun(void)
 {
-    printf ("You're beat! and yet, you must allow,\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
