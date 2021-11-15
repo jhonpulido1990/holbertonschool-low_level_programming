845a0a119beaa5f149d48bbfde591e23cc798f3c
jhonpulido1990 <3825@holbertonschool.com>
2021-11-14 20:12:14 -0500

    correction structure of file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,17 +11,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-			h = h->next;
-			a++;
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			a++;
-		}
+		h = h->next;
+		a++;
 	}
 	return (a);
 }
