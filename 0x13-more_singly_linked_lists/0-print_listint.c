80726d55ee0181e8f96ae04dcbee22338f350bba
jhonpulido1990 <3825@holbertonschool.com>
2021-11-16 10:57:23 -0500

    change in output

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		if (h->n == '\0')
-			printf("(nil)\n");
-		else
-			printf("%d\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		n++;
 	}
