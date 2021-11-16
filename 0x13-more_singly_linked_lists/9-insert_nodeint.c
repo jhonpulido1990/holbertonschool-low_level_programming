fac3d46c83f7fa6d901e090a1811208f6dcfbf0d
jhonpulido1990 <3825@holbertonschool.com>
2021-11-16 13:04:46 -0500

    add idx when is 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,12 +11,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newNode, *temp;
 	unsigned int a = 0;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 	{
 		return (NULL);
 	}
 	temp = *head;
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
 	while (a < idx - 1)
 	{
 		temp = temp->next;
@@ -25,5 +35,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newNode->n = n;
 	newNode->next = temp->next;
 	temp->next = newNode;
+
 	return (newNode);
 }
