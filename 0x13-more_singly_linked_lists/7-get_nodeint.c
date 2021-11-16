4b944415393d51cd52872bddbdbe720a2df2b095
jhonpulido1990 <3825@holbertonschool.com>
2021-11-16 12:47:44 -0500

    change conditional of head, into while

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,17 +9,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int a = 0;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-	if (index == 0)
-	{
-		return (0);
-	}
 	while (a < index)
 	{
+		
 		head = head->next;
+		if (head == NULL)
+		{
+			return (NULL);
+		}
 		a++;
 	}
 	return (head);
