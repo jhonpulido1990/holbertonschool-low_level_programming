4b8ab8bcc8f55c97a2ffdd79b98ab62ba8f6fa32
jhonpulido1990 <3825@holbertonschool.com>
2021-11-16 09:49:11 -0500

    fix if n is null

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,7 +10,10 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		if (h->n == '\0')
+			printf("(nill)");
+		else
+			printf("%d\n", h->n);
 		h = h->next;
 		n++;
 	}
