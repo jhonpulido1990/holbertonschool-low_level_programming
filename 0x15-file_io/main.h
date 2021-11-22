f1734346ba1a951d0a3f5aca1f1a8acc385fef94
jhonpulido1990 <3825@holbertonschool.com>
2021-11-22 13:03:32 -0500

    header, putchar and readme

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*include library*/
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+/*call putchar*/
+int _putchar(char c);
+/*converts a binary number to an unsigned int*/
+unsigned int binary_to_uint(const char *b);
+/*prints the binary representation of a number*/
+void print_binary(unsigned long int n);
+/*returns the value of a bit at a given index*/
+int get_bit(unsigned long int n, unsigned int index);
+/*sets the value of a bit to 1 at a given index*/
+int set_bit(unsigned long int *n, unsigned int index);
+/*sets the value of a bit to 0 at a given index*/
+int clear_bit(unsigned long int *n, unsigned int index);
+/*number of bits you would need to flip to get from one number to another*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+/*function that checks the endianness*/
+int get_endianness(void);
+
+#endif /* MAIN_H */
