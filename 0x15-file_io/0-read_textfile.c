e3d099aedd3c783578572869d6579f7f1a7d1356
jhonpulido1990 <3825@holbertonschool.com>
2021-11-23 08:51:31 -0500

    reverse

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, count1, count;
 	char *buffer;
 
-	if (filename == NULL | !letters)
+	if (filename == NULL)
 	{
 		return (0);
 	}
