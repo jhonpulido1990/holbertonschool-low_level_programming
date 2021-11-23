414e35c7651daefb70f5cb66d493528a34379a87
jhonpulido1990 <3825@holbertonschool.com>
2021-11-23 14:16:46 -0500

    correct output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,17 +13,25 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR);
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (fd == -1)
 	{
 		close(fd);
 		return (-1);
 	}
-	/*read(fd, text_content, strlen(text_content));*/
-	count = write(fd, text_content, strlen(text_content));
-	if (count == -1)
+	else
 	{
-		return (-1);
+		if (text_content == NULL)
+		{
+			close(fd);
+			return (1);
+		}
+		count = write(fd, text_content, strlen(text_content));
+		close(fd);
+		if (count == -1)
+		{
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
