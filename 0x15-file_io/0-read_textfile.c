512b45dd72644a402328ae3c97b15448ac465625
jhonpulido1990 <3825@holbertonschool.com>
2021-11-23 11:57:53 -0500

    correct ouput

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,12 +12,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 	buffer = malloc(sizeof(char *) * letters);
 	if (buffer == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
