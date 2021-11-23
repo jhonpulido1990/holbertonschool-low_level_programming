9af4488d541884f135631fa88dae65df11b7a06f
jhonpulido1990 <3825@holbertonschool.com>
2021-11-23 08:48:13 -0500

    add close if read and write faul

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -27,8 +27,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	count = read(fd, buffer, letters);
 	count1 = write(1, buffer, letters);
-	if (count1 == -1)
+	if ((count1 == -1) | (count == -1))
 	{
+		close(fd);
 		return (0);
 	}
 	close(fd);
