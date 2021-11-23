fe1533f7592193b889acfa0fd1f50b488c6a3c95
jhonpulido1990 <3825@holbertonschool.com>
2021-11-23 17:17:45 -0500

    change in function open

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_APPEND | O_WRONLY, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
 		close(fd);
@@ -23,6 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		if (text_content == NULL)
 		{
+			close(fd);
 			return (-1);
 		}
 		size = strlen(text_content);
