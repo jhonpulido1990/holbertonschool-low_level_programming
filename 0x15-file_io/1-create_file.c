2f0e1d206cce8279af1efb58039ee2a9c68a06a6
jhonpulido1990 <3825@holbertonschool.com>
2021-11-23 11:08:00 -0500

    add comment

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * @brief 
- * 
+ * create_file - Create a function that creates a file
+ * @filename: name file
+ * @text_content: string to print
+ * Return: 1 or -1
  */
 int create_file(const char *filename, char *text_content)
 {
