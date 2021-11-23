cb51152cd1be8725f8a3a3ae9a648681c5c556b7
jhonpulido1990 <3825@holbertonschool.com>
2021-11-23 12:01:34 -0500

    reverse

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,18 +12,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 	{
-		return (-1);
+		return (0);
 	}
 	buffer = malloc(sizeof(char *) * letters);
 	if (buffer == NULL)
 	{
-		return (-1);
+		return (0);
 	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		close(fd);
-		return (-1);
+		return (0);
 	}
 	count = read(fd, buffer, letters);
 	count1 = write(1, buffer, letters);
