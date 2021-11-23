44d9b5878f36332a1ab35c43cb801ac885d4d20c
jhonpulido1990 <3825@holbertonschool.com>
2021-11-23 08:50:57 -0500

    add if letter faul

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, count1, count;
 	char *buffer;
 
-	if (filename == NULL)
+	if (filename == NULL | !letters)
 	{
 		return (0);
 	}
