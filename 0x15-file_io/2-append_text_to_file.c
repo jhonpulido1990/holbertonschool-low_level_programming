f5f6148a0efb95f558ff135cf0d66e5b52ae2bb5
jhonpulido1990 <3825@holbertonschool.com>
2021-11-23 15:02:59 -0500

    add comment

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * @brief 
- * 
+ * append_text_to_file - copies the content of a file to another file
+ * @filename: name file output
+ * @text_content: string to copy
+ * Return: 1 or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -17,12 +19,19 @@ int append_text_to_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
-	size = strlen(text_content);
-	count = write(fd, text_content, size);
-	if (count == -1)
+	else
 	{
-		return (-1);
+		if (text_content == NULL)
+		{
+			return (-1);
+		}
+		size = strlen(text_content);
+		count = write(fd, text_content, size);
+		if (count == -1)
+		{
+			return (-1);
+		}
+		close(fd);
 	}
-	close(fd);
 	return (1);
 }
