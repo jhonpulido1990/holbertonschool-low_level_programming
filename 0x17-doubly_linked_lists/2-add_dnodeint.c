bdf7e8f30feb01f05758eb222e307efb886d406b
jhonpulido1990 <3825@holbertonschool.com>
2022-01-06 15:39:54 -0500

    task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add data to the list
+ * @head: likend list
+ * @n: data to add
+ * Return: new likend list
+ *
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_list;
+
+	if (head == NULL)
+		return (NULL);
+	new_list = malloc(sizeof(dlistint_t));
+	if (new_list == NULL)
+		return (NULL);
+	new_list->n = n;
+	new_list->next = *head;
+	new_list->prev = NULL;
+	*head = new_list;
+	return (new_list);
+}
