f1283104824f502972619114e1c0ce83c9c319a2
jhonpulido1990 <3825@holbertonschool.com>
2022-01-06 23:59:59 -0500

    task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - delecte node in position of index
+ * @head: likend list
+ * @index: position of node
+ * Return: delecte node
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current;
+    unsigned int i = 0;
+
+    if (*head == NULL)
+        return (-1);
+
+    current = *head;
+
+    while (i < index)
+    {
+        current = current->next;
+        if (current == NULL)
+            return (-1);
+        i++;
+    }
+    if (current == *head)
+        *head = current->next;
+
+    if (current->next != NULL)
+        current->next->prev = current->prev;
+
+    return (1);
+}
