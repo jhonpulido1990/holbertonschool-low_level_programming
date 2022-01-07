262c03a12c26007ea3ec2b0f614fab1dd441c381
jhonpulido1990 <3825@holbertonschool.com>
2022-01-07 09:11:17 -0500

    if currect prev is NULL

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -27,6 +27,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (current->next != NULL)
 		current->next->prev = current->prev;
+	
+	if (current->prev != NULL)
+        current->prev->next = current->next;
 
 	return (1);
 }
