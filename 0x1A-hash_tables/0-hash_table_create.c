f65ca42f7c8217b9ff7dcdd848fb69f512c54304
jhonpulido1990 <3825@holbertonschool.com>
2022-01-28 10:23:54 -0500

    create location to node

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,9 +8,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *array = NULL;
+	hash_node_t *node = NULL;
 
 	array = malloc(sizeof(hash_table_t) * size);
 	if (array == NULL)
+		return (NULL);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 		return (NULL);
 	return (array);
 }
