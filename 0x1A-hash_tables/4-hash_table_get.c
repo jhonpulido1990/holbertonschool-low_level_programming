1b56087fee448ef658657378bf2cf5804561577e
jhonpulido1990 <3825@holbertonschool.com>
2022-01-28 16:40:24 -0500

    reverse code

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,6 +10,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int index = 0;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index])
 	{
