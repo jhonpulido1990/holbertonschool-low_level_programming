e50fbbdbf6e769d10ef585402bbf9b39e1be9faf
jhonpulido1990 <3825@holbertonschool.com>
2022-01-28 18:32:33 -0500

    correctio form print

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -17,7 +17,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			while (tmp != NULL)
 			{
-				printf("'%s' : '%s'", tmp->key, tmp->value);
+				printf("'%s' : '%s', ", tmp->key, tmp->value);
 				tmp = tmp->next;
 			}
 		}
