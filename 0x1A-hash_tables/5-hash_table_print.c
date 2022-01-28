1d025212beb42f78a30741aa41801fd290839240
jhonpulido1990 <3825@holbertonschool.com>
2022-01-28 18:29:48 -0500

    correction space

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print
+ *
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	unsigned long int i = 0;
+
+	printf("{");
+	for (i = 0; i <= ht->size; i++)
+	{
+		tmp = ht->array[i];
+		if (tmp != NULL)
+		{
+			while (tmp != NULL)
+			{
+				printf("'%s' : '%s'", tmp->key, tmp->value);
+				tmp = tmp->next;
+			}
+		}
+	}
+	printf("}\n");
+}
