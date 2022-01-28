17a9ad5663002fa8bc079a2ca10fcc34dd5c68bb
jhonpulido1990 <3825@holbertonschool.com>
2022-01-28 16:57:20 -0500

    correct evaluation in likend list

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,16 +8,16 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *tmp = NULL;
 	int index = 0;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index])
+	for (tmp = ht->array[index]; tmp != NULL; tmp = tmp->next)
 	{
 		if (strcmp(ht->array[index]->key, key) == 0)
 			return (ht->array[index]->value);
-		ht->array[index] = ht->array[index]->next;
 	}
 	return (NULL);
 }
