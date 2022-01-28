9ad397cbf1101f650a5a2bdc108bcbb86be2568c
jhonpulido1990 <3825@holbertonschool.com>
2022-01-28 16:39:32 -0500

    correct output

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,8 +10,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int index = 0;
 
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index])
 	{
