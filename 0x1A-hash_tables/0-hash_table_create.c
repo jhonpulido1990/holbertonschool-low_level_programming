01d6b7180c4d6a366dbd3f1b7a4b3af3ac4798b6
jhonpulido1990 <3825@holbertonschool.com>
2022-01-28 10:28:50 -0500

    add if size is 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,11 +10,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *array = NULL;
 	hash_node_t *node = NULL;
 
-	array = malloc(sizeof(hash_table_t) * size);
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(*array) * size);
 	if (array == NULL)
 		return (NULL);
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
+	{
+		free(array);
 		return (NULL);
+	}
 	return (array);
 }
