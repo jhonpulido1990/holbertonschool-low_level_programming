ddc4b6af0d4b50401aae78d279a0ddb9e4a8660c
jhonpulido1990 <3825@holbertonschool.com>
2022-01-28 22:39:15 -0500

    correct style betty

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,15 +1,15 @@
 #include "hash_tables.h"
 /**
- * @brief
+ * hash_table_delete - delecte list
  *
- * @param ht
+ * @ht: free - list
  */
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *next;
 	hash_node_t *list;
 	unsigned long int i = 0;
-	
+
 	for (i = 0; i < ht->size; i++)
 	{
 		list = ht->array[i];
