d726e3b0828275f96d72279301564bab562653f5
jhonpulido1990 <3825@holbertonschool.com>
2022-03-21 21:58:42 -0500

    add column and row

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,34 +6,35 @@
 
 def island_perimeter(grid):
     """ Perimeter """
-    h = 0
-    v = 0
     perime = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            derecha = h + 1
-            izquierda = h - 1
-            arriba = v - 1
-            abajo = v + 1
+            derecha = j + 1
+            izquierda = j - 1
+            arriba = i - 1
+            abajo = i + 1
             if grid[i][j] == 1:
-                if izquierda > 0:
+                if izquierda > -1:
                     if grid[i][izquierda] == 0:
                         perime += 1
                 else:
                     perime += 1
-                if derecha < j:
+                if derecha < len(grid[i]):
                     if grid[i][derecha] == 0:
                         perime += 1
                 else:
                     perime += 1
-                if arriba > 0:
+                if arriba > -1:
                     if grid[arriba][j] == 0:
                         perime += 1
                 else:
                     perime += 1
-                if abajo < i:
+                if abajo < len(grid):
                     if grid[abajo][j] == 0:
                         perime += 1
                 else:
                     perime += 1
-    return perime - 4
+    if perime == -4:
+        return 0
+    else:
+        return perime
