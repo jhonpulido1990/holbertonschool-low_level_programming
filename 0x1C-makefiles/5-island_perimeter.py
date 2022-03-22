1de526f724657222c7bf76dfb427dab06fbb4fb2
jhonpulido1990 <3825@holbertonschool.com>
2022-03-21 21:15:41 -0500

    add comment

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,30 +5,35 @@
 
 
 def island_perimeter(grid):
-    h = 0
-    v = 0
-    perime = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            derecha = h + 1
-            izquierda = h - 1
-            arriba = v - 1
-            abajo = v + 1
-            if grid[i][j] == 1:
-                if izquierda > -1:
-                    if grid[i][izquierda] == 0:
-                        perime += 1
-                else:
-                    perime += 1
-                if derecha < j:
-                    if grid[i][derecha] == 0:
-                        perime += 1
-                if arriba > -1:
-                    if grid[arriba][j] == 0:
-                        perime += 1
-                else:
-                    perime += 1
-                if abajo < i:
-                    if grid[abajo][j] == 0:
-                        perime += 1
-    return perime
+	""" Perimeter """
+	h = 0
+	v = 0
+	perime = 0
+	for i in range(len(grid)):
+		for j in range(len(grid[i])):
+			derecha = h + 1
+			izquierda = h - 1
+			arriba = v - 1
+			abajo = v + 1
+			if grid[i][j] == 1:
+				if izquierda > -1:
+					if grid[i][izquierda] == 0:
+						perime += 1
+				else:
+					perime += 1
+				if derecha < j:
+					if grid[i][derecha] == 0:
+						perime += 1
+				else:
+					perime += 1
+				if arriba > -1:
+					if grid[arriba][j] == 0:
+						perime += 1
+				else:
+					perime += 1
+				if abajo < i:
+					if grid[abajo][j] == 0:
+						perime += 1
+				else:
+					perime += 1
+	return perime - 4
